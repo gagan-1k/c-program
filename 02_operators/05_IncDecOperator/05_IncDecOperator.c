5dc6e2dba733864519437b7e652fc49ba1631464
gagan-1k <127132913+gagan-1k@users.noreply.github.com>
2023-04-10 07:17:06 +0530

    Add files via upload

---
+++ 02_operators/05_IncDecOperator/05_IncDecOperator.c
@@ -0,0 +1,38 @@
+/*********************************************************************************************
+ * © 2022 RuggedBOARRD. - All Rights Reserved. Permission to use, modify, copy, and distribute
+ * this source code, object code, or executable code (collectively, Software), is granted only
+ * under the terms of a valid written license agreement with RuggedBOARD. Unauthorized copying
+ * or other use of the Software is strictly prohibited.  Software is owned by and constitutes
+ * the proprietary works, trade secrets, and copyrights of Embitel or its licensors.
+ * For further information, contact community.ruggedboard.com / info@ruggedboard.com
+ *
+ * Application: 
+ * Brief: 
+ * Author: Gagandeep r naik  ( gagandeepnaik17@gmail.com )
+ * Title: Project Engineer
+ * Last Modified Date: 23.03.2023
+ *
+/*********************************************************************************************/
+
+// Header File
+#include <stdio.h>
+
+// Main Function
+
+int main() 
+{
+    int a = 10;
+
+    printf("Initial value of a: %d\n", a);
+
+    a++; // increment operator
+    printf("Value of a after increment: %d\n", a);
+
+    a--; // decrement operator
+    printf("Value of a after decrement: %d\n", a);
+
+    return 0;
+}
+
+
+// Program End
